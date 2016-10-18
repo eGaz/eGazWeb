764729371286ec80f090a2f534b121b45753a91e
Wilker Mesquita <wilker-mc@hotmail.com>
2016-10-18 11:58:41 -0200

    Adding roles with alanning

---
+++ egazweb/imports/api/groups.js
@@ -0,0 +1,15 @@
+import { Mongo } from 'meteor/mongo';
+
+/*export const Groups = new Mongo.Collection('groups');
+
+Lists.schema = new Groups({
+  id: {type:  Number},
+  name: {type: String},
+  description: {type: String};
+});
+
+Groups.helpers({
+  user() {
+    return User.find({});
+  }
+});*/
---
+++ egazweb/imports/api/user-custom-info.js
@@ -0,0 +1,7 @@
+import { Mongo } from 'meteor/mongo';
+
+/*export const CustomInfo = new Mongo.Collection('custom-infos');
+
+Lists.schema = new CustomInfo({
+  username: Meteor.user().username,
+});*/
