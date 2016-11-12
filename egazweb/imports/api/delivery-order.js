4cf3d5a558521c24efbe7cb26404d27762a8d9a0
wilkerwma <wilker-mc@hotmail.com>
2016-11-12 10:39:21 -0200

    Delivery orders

--- egazweb/imports/api/delivery-order.js
+++ egazweb/imports/api/delivery-order.js
@@ -2,8 +2,28 @@ import { Mongo } from 'meteor/mongo';
 
 export const DeliveryOrder = new Mongo.Collection('delivery-order');
 
-DeliveryOrder.schema = new SimpleSchema({
-    neighborhood: {type: String},
-    adress: {type: String},
-    number: {type: Number}
+DeliveryOrder.attachSchema(new SimpleSchema({
+  neighborhood: {type: String},
+  address: {type: String},
+  number: {type: Number},
+  deliveryMan: {type: [String],  defaultValue: []},
+}));
+
+Meteor.methods({
+    'createDeliveryOrder': function(neighborhood, address, number){
+      check(neighborhood, String);
+      check(address, String);
+      check(number, Number);
+
+      if (! this.userId) {
+        throw new Meteor.Error('not-authorized');
+      }
+
+      DeliveryOrder.insert({
+        neighborhood,
+        address,
+        number,
+        createdAt: new Date(),
+      });
+    },
 });
